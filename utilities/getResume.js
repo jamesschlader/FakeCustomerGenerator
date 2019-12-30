9068241e39ac5cd5266b3cfb725817023d22bd00
Jim Schlader <jamesschlader@yahoo.com>
2019-12-29 19:13:15 -0700

    changed the get resume field names

--- utilities/getResume.js
+++ utilities/getResume.js
@@ -1,16 +1,15 @@
 const faker = require("faker");
 
-const getResume = ()=>{
-   
-    const job = {};
-    job.company = faker.company.companyName();
-    const oneDate = faker.date.past();
-    const anotherDate = faker.date.past();
-    job.from = oneDate <= anotherDate ? oneDate : anotherDate;
-    job.to = oneDate <= anotherDate ? anotherDate : oneDate;
-    job.description = faker.lorem.sentence();
+const getResume = () => {
+  const job = {};
+  job.company = faker.company.companyName();
+  const oneDate = faker.date.past();
+  const anotherDate = faker.date.past();
+  job.fromValue = oneDate <= anotherDate ? oneDate : anotherDate;
+  job.toValue = oneDate <= anotherDate ? anotherDate : oneDate;
+  job.description = faker.lorem.sentence();
 
-    return job
-}
+  return job;
+};
 
-module.exports = getResume
+module.exports = getResume;
