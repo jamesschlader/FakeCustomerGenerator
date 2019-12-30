164ae7743dbb1de614fb7e6e1b7d44bf0c5b8a15
Schlader <James.Schlader@henryschein.com>
2019-12-29 18:26:55 -0700

    updated the get resume function

--- utilities/getResume.js
+++ utilities/getResume.js
@@ -6,8 +6,8 @@ const getResume = ()=>{
     job.company = faker.company.companyName();
     const oneDate = faker.date.past();
     const anotherDate = faker.date.past();
-    job.from = oneDate <= anotherDate ? oneDate : anotherDate;
-    job.to = oneDate <= anotherDate ? anotherDate : oneDate;
+    job.fromValue = oneDate <= anotherDate ? oneDate : anotherDate;
+    job.toValue = oneDate <= anotherDate ? anotherDate : oneDate;
     job.description = faker.lorem.sentence();
 
     return job
