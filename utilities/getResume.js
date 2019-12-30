4ba2a0cfe3281da6ed05ceb12e53c4722a691b58
Schlader <James.Schlader@henryschein.com>
2019-12-29 17:48:06 -0700

    send back one job at a time

--- utilities/getResume.js
+++ utilities/getResume.js
@@ -1,23 +1,16 @@
 const faker = require("faker");
 
 const getResume = ()=>{
-    let history = {};
+   
+    const job = {};
+    job.company = faker.company.companyName();
+    const oneDate = faker.date.past();
+    const anotherDate = faker.date.past();
+    job.from = oneDate <= anotherDate ? oneDate : anotherDate;
+    job.to = oneDate <= anotherDate ? anotherDate : oneDate;
+    job.description = faker.lorem.sentence();
 
-    const jobs = Math.floor(Math.random() * 3) + 1;
-
-    for (let i = 1; i < jobs + 1; i++){
-        const job = {};
-        job.company = faker.company.companyName();
-        const oneDate = faker.date.past();
-        const anotherDate = faker.date.past();
-        job.from = oneDate <= anotherDate ? oneDate : anotherDate;
-        job.to = oneDate <= anotherDate ? anotherDate : oneDate;
-        job.description = faker.lorem.sentence();
-
-        history[`job${i}`] = job;
-    }
-
-    return history
+    return job
 }
 
 module.exports = getResume
