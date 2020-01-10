01b8f7f009f95c42ff0c245eefc1793a8cf05eac
James.Schlader <jamesschlader@yahoo.com>
2020-01-10 12:59:01 -0700

    fixed a typo

--- utilities/getOne.js
+++ utilities/getOne.js
@@ -1,16 +1,16 @@
 const faker = require("faker");
 
-const getOne = ()=>{
-    const firstName = faker.name.firstName();
-    const lastName = fker.name.lastName();
-    const email = faker.internet.email();
-    const address = faker.address.streetAddress();
-    const city = faker.address.city();
-    const state = faker.address.state();
-    const zip = faker.address.zipCode();
-    const image = faker.internet.avatar();
-    const phone = faker.phone.phoneNumber()
-    return `"${lastName}, ${firstName}", "${email}", "${image}", "${address}", "${city}, ${state}", ${zip}, ${phone}\n`
-}
+const getOne = () => {
+	const firstName = faker.name.firstName();
+	const lastName = faker.name.lastName();
+	const email = faker.internet.email();
+	const address = faker.address.streetAddress();
+	const city = faker.address.city();
+	const state = faker.address.state();
+	const zip = faker.address.zipCode();
+	const image = faker.internet.avatar();
+	const phone = faker.phone.phoneNumber();
+	return `"${lastName}, ${firstName}", "${email}", "${image}", "${address}", "${city}, ${state}", ${zip}, ${phone}\n`;
+};
 
-module.exports = getOne
+module.exports = getOne;
