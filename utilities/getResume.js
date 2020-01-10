bb9028e407106fa5ff489c86e762fd3c02ed095c
James.Schlader <jamesschlader@yahoo.com>
2020-01-10 15:09:32 -0700

    the resume route now returns a list of resumes

--- utilities/getResume.js
+++ utilities/getResume.js
@@ -1,15 +1,20 @@
 const faker = require("faker");
 
 const getResume = () => {
-	const job = {};
-	job.company = faker.company.companyName();
-	const oneDate = faker.date.past();
-	const anotherDate = faker.date.past();
-	job.fromValue = oneDate <= anotherDate ? oneDate : anotherDate;
-	job.toValue = oneDate <= anotherDate ? anotherDate : oneDate;
-	job.description = faker.lorem.sentence();
+	const number = Math.floor(Math.random() * 5 + 1);
+	let resumes = [];
 
-	return job;
+	for (let i = 0; i < number; i++) {
+		const job = {};
+		job.company = faker.company.companyName();
+		const oneDate = faker.date.past();
+		const anotherDate = faker.date.past();
+		job.fromValue = oneDate <= anotherDate ? oneDate : anotherDate;
+		job.toValue = oneDate <= anotherDate ? anotherDate : oneDate;
+		job.description = faker.lorem.sentence();
+		resumes.push(job);
+	}
+	return resumes;
 };
 
 module.exports = getResume;
