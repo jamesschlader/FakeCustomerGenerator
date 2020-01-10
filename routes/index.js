78209217fdcd059e997728b9c9c041e96b460618
James.Schlader <jamesschlader@yahoo.com>
2020-01-10 12:33:50 -0700

    Added a route just to get an image

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,49 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 const faker = require("faker");
-const getOne = require("../utilities/getOne")
-const getMany = require("../utilities/getMany")
-const getResume = require("../utilities/getResume")
+const getOne = require("../utilities/getOne");
+const getMany = require("../utilities/getMany");
+const getResume = require("../utilities/getResume");
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  const firstName = faker.name.firstName();
-  const lastName = faker.name.lastName();
-  const email = faker.internet.email();
-  const address = faker.address.streetAddress();
-  const city = faker.address.city();
-  const state = faker.address.state();
-  const zip = faker.address.zipCode();
-  const image = faker.internet.avatar();
-  const phone = faker.phone.phoneNumber();
+router.get("/", (req, res, next) => {
+	const firstName = faker.name.firstName();
+	const lastName = faker.name.lastName();
+	const email = faker.internet.email();
+	const address = faker.address.streetAddress();
+	const city = faker.address.city();
+	const state = faker.address.state();
+	const zip = faker.address.zipCode();
+	const image = faker.internet.avatar();
+	const phone = faker.phone.phoneNumber();
 
-  res.send({lastName, firstName, email, address, city, state, zip, image, phone});
+	res.send({
+		lastName,
+		firstName,
+		email,
+		address,
+		city,
+		state,
+		zip,
+		image,
+		phone
+	});
 });
 
-router.get("/csv", (req, res, next) =>{
-  res.send(getOne())
-})
+router.get("/csv", (req, res, next) => {
+	res.send(getOne());
+});
 
 router.get("/csv/:number", (req, res, next) => {
-  res.send(getMany(req.params.number))
-})
+	res.send(getMany(req.params.number));
+});
 
-router.get("/resume", (req, res, next) =>{
-  res.send(getResume())
-})
+router.get("/resume", (req, res, next) => {
+	res.send(getResume());
+});
+
+router.get("/image", (res, req, next) => {
+	res.send(getImage());
+});
 
 module.exports = router;
